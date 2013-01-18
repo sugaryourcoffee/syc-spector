814d166027625a1d6d72d8048020d6541e28d78b
Pierre <pierre@sugaryourcoffee.de>
2013-01-19 00:16:52 +0100

    add default pattern that is used if no pattern is provided

--- inspector/lib/inspector/pattern.rb
+++ inspector/lib/inspector/pattern.rb
@@ -6,6 +6,9 @@ EMAIL_PATTERN = /\A[\w!#\$%&'*+\/=?`{|}~^-]+(?:\.[\w!#\$%&'*+\/=?`{|}~^-]+)*@(?:
 # Pattern that relizes an email within a line.
 ANY_EMAIL_PATTERN = /[\w!#\$%&'*+\/=?`{|}~^-]+(?:\.[\w!#\$%&'*+\/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}/
 
+# Default pattern scanning whole lines
+DEFAULT_PATTERN = /\A.*\Z/
+
 # Pattern that relizes strings between \A and \Z that is beginning of line and
 # end of line patterns.
 FULL_LINE = /(?<=\\A).*(?=\\Z)/
